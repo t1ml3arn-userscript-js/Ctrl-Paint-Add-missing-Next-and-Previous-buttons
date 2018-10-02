8ff455d7b52e6f180f94d0ee8fb85ea841e8e4f5
Tim <T1mL3arn@users.noreply.github.com>
2018-10-02 23:02:35 +1000

    initial commit

---
+++ ctrlpaint+.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name ctrlpaint site improver
+// @namespace 
+// @description Adds next/prev buttons for some tutorial series
+// @author T1m
+// @version 0.1
+// @icon 
+// @match https://ctrlpaint.com/*
+// @exclude-match 
+// @require 
+// @resource 
+// @run-at document-end
+// @noframes
+// @grant none
+// @license GPLv3 
+// @homepageURL 
+// @supportURL 
+// @downloadURL 
+// @updateURL 
+// ==/UserScript==
